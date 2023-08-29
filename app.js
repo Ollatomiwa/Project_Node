0edca77f2b87b9817235f613f0b790fb23dc3ce4
ollatomiwa <ajaomoshood@gmail.com>
2023-08-29 20:57:22 +0100

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const _ = require ('loadash');
+
 
 const items = [1,[2, [3, [4]]]];
-const newItems = items.flatMap
+const newItems = items.length
 console.log(newItems);
+console.log("good morning");
